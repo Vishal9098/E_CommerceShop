8e9a2a33473e1f960166f2d6c9af80bb8fbdf1a7
Vishal9098 <vishalsilawat6@gmail.com>
2024-11-01 00:56:57 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+
+
+
+# Create your models here.
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+# Create your models here.
+
+STATE_CHOICES = (
+    ('Andaman & Nicobar Island','Andaman & Nicobar Island'),
+    ('Andhra Pradesh','Andhra Pradesh'),
+    ('Assam','Assam'),
+    ('Bihar','Bihar'),
+    ('Chandigarh','Chandigarh'),
+    ('Chhattissgarh','Chhattissgarh'),
+    ('Delhi','Delhi'),
+    ('Goa','Goa'),
+    ('Gujarat','Gujarat'),
+    ('Haryana','Haryana'),
+    
+    
+    
+)
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=50)
+    
+    def __str__(self):
+        return str(self.id)
+    
+CATEGORY_CHOICES = (
+    ('M', 'Mobile'),
+    ('L', 'Laptop'),
+    ('TW', 'Top Wear'),
+    ('BW', 'Bottom Wear'),
+    
+)
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    brand = models.CharField(max_length=100)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='productimg')
+    
+    def __str__(self):
+        return str(self.id)
+    
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity  = models.PositiveIntegerField(default=1)
+    
+    def __str__(self):
+        return str(self.id)
+
+STATE_CHOICES = (
+    ('Accepted','Accepted'),
+    ('Packed','Packed'),
+    ('On The Way','On The Way'),
+    ('Cancel','Cancel')
+)
+
+class OrderPlaced(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity =  models.PositiveIntegerField(default=1)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50,
+    choices=STATE_CHOICES,default='Pending')
+    
---
+++ app/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path 
+from app import views 
+from django.conf import settings 
+from django.conf.urls.static import static 
+from django.contrib.auth import views as auth_views 
+from .forms import LoginForm , MyPasswordChangeForm
+# from .views import CustomerRegistrationView
+urlpatterns = [
+    path('', views.ProductView.as_view(), name='home'),
+    path('product-detail/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
+    path('cart/', views.add_to_cart, name='add-to-cart'),
+    path('buy/', views.buy_now, name='buy-now'),
+    path('profile/', views.profile, name='profile'),
+    path('address/', views.address, name='address'),
+    path('orders/', views.orders, name='orders'),
+    # path('changepassword/', views.change_password, name='changepassword'),
+    path('mobile/', views.mobile, name='mobile'),
+    path('mobile/<slug:data>', views.mobile, name='mobiledata'),
+
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
+    # path('accountss/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
+    path('accounts/login/', auth_views.LogoutView.as_view(next_page='accounts/login/'), name='accounts/login/'),
+    path('passwordchange/', auth_views.PasswordChangeView.as_view(template_name='app/passwordchange.html',form_class=MyPasswordChangeForm), name='passwordchange'),
+    path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
+    path('checkout/', views.checkout, name='checkout'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
